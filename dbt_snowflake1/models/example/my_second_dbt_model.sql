c1da10909aafa7829b794fdd0b0803c2edd0b020
vu <vu@vus-MacBook-Pro.local>
2023-06-07 00:58:07 +0700

    try with snapshot

--- dbt_snowflake1/models/example/my_second_dbt_model.sql
+++ dbt_snowflake1/models/example/my_second_dbt_model.sql
@@ -1,6 +1,6 @@
 
 -- Use the `ref` function to select from other models
 
-select *
+select *, 'HN' as code, '2020-03-01 00:00:00.000'::timestamp as updated_at
 from {{ ref('my_first_dbt_model') }}
 where id = 1
